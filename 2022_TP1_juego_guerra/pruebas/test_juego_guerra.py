2cb2c4b5d1fc2a314d026a6e5989447295da0a54
DianaVertiz <jesuisdianacarol@gmail.com>
2022-09-08 10:23:34 -0300

    Modificación de testing para el juego Guerra

--- 2022_TP1_juego_guerra/pruebas/test_juego_guerra.py
+++ 2022_TP1_juego_guerra/pruebas/test_juego_guerra.py
@@ -15,23 +15,23 @@ class TestJuegoGuerra(unittest.TestCase):
     
     def setUp(self):
         
-        '''jugador 1 gana la partida en el turno 28'''
-        self.juego_1 = JuegoGuerra(random_seed= 547)
-        '''jugador 1 gana la partida en el turno 432'''
-        self.juego_2 = JuegoGuerra(random_seed=314)
-        '''jugador 1 gana la partida en el turno 1754'''
-        self.juego_3 = JuegoGuerra(random_seed=59)
+        '''jugador 1 gana la partida en el turno 137'''
+        self.juego_1 = JuegoGuerra(random_seed= 314)
+        '''jugador 1 gana la partida en el turno 638'''
+        self.juego_2 = JuegoGuerra(random_seed=59)
+        '''jugador 1 gana la partida en el turno 1383'''
+        self.juego_3 = JuegoGuerra(random_seed=883)
                 
-        '''jugador 2 gana la partida en el turno 220'''
-        self.juego_4 = JuegoGuerra(random_seed=10)                
-        '''jugador 2 gana la partida en el turno 1290'''
-        self.juego_5 = JuegoGuerra(random_seed=408)
-        '''jugador 2 gana la partida en el turno 3149 por Guerra'''
-        self.juego_6 = JuegoGuerra(random_seed=572)
+        '''jugador 2 gana la partida en el turno 145'''
+        self.juego_4 = JuegoGuerra(random_seed=167)                
+        '''jugador 2 gana la partida en el turno 1112'''
+        self.juego_5 = JuegoGuerra(random_seed=190)
+        '''jugador 2 gana la partida en el turno 1373 por Guerra'''
+        self.juego_6 = JuegoGuerra(random_seed=735)
         
         '''juego empate'''
-        self.juego_7 = JuegoGuerra(random_seed=18)        
-        self.juego_8 = JuegoGuerra(random_seed=404)
+        self.juego_7 = JuegoGuerra(random_seed=547)        
+        self.juego_8 = JuegoGuerra(random_seed=296)
     
     def test_resulta_gana_jugador1(self):
         """compruebo el número de turnos de 3 partidas con
@@ -41,13 +41,13 @@ class TestJuegoGuerra(unittest.TestCase):
         self.juego_2.iniciar_juego()
         self.juego_3.iniciar_juego()
         
-        self.assertEqual(self.juego_1.turnos_jugados, 28)
+        self.assertEqual(self.juego_1.turnos_jugados, 137)
         self.assertEqual(self.juego_1.ganador, 'jugador 1')
         
-        self.assertEqual(self.juego_2.turnos_jugados, 432)
+        self.assertEqual(self.juego_2.turnos_jugados, 638)
         self.assertEqual(self.juego_2.ganador, 'jugador 1')
         
-        self.assertEqual(self.juego_3.turnos_jugados, 1754)
+        self.assertEqual(self.juego_3.turnos_jugados, 1383)
         self.assertEqual(self.juego_3.ganador, 'jugador 1')
         
     
@@ -59,13 +59,13 @@ class TestJuegoGuerra(unittest.TestCase):
         self.juego_5.iniciar_juego()
         self.juego_6.iniciar_juego()
         
-        self.assertEqual(self.juego_4.turnos_jugados, 220)
+        self.assertEqual(self.juego_4.turnos_jugados, 145)
         self.assertEqual(self.juego_4.ganador, 'jugador 2')
         
-        self.assertEqual(self.juego_5.turnos_jugados, 1290)
+        self.assertEqual(self.juego_5.turnos_jugados, 1112)
         self.assertEqual(self.juego_5.ganador, 'jugador 2')
         
-        self.assertEqual(self.juego_6.turnos_jugados, 3149)
+        self.assertEqual(self.juego_6.turnos_jugados, 1373)
         self.assertEqual(self.juego_6.ganador, 'jugador 2')
     
     def test_resulta_empate(self):
