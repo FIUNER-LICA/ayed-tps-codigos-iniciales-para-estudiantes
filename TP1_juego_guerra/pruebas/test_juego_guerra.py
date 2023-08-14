d637cc7ee2e47bd48f9d93f42f34e0ddc08b702f
JonaNicolet <135644805+JonaNicolet@users.noreply.github.com>
2023-08-14 14:27:05 -0300

    Update test_juego_guerra.py
    Se agregaron pruebas para el mazo en operaciones de poner y sacar cartas

--- TP1_juego_guerra/pruebas/test_juego_guerra.py
+++ TP1_juego_guerra/pruebas/test_juego_guerra.py
@@ -79,7 +79,22 @@ class TestJuegoGuerra(unittest.TestCase):
         
         self.assertTrue(self.juego_7.empate)
         self.assertTrue(self.juego_8.empate)
-    
-    
+
+class TestMazo(unittest.TestCase):
+    def setUp(self):
+        self.mazo= Mazo()
+
+# Chequeo de repartir/poner arriba, sacar arriba/principio y poner abajo/final
+    def test_poner_sacar_arriba(self):            #Poner arriba al momento de repartir las cartas, sacar arriba al momento de jugar el turno
+        carta1=Carta('5','trebol')
+        self.mazo.poner_arriba(carta1)
+        self.assertIs(carta1, self.mazo.mazo.items[0])
+        carta2=self.mazo.sacar_arriba()
+        self.assertIs(carta1, carta2)
+
+    def test_poner_abajo(self):                   #Poner abajo al momento de ganar el turno
+        
+        
+        
 if __name__ == '__main__':
     unittest.main()
