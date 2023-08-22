68c7dc8bbbb6054d1f2ffa8d215b9ce6139047cd
JonaNicolet <135644805+JonaNicolet@users.noreply.github.com>
2023-08-22 16:08:09 -0300

    Se agrega test para poner_abajo

--- TP1_juego_guerra/pruebas/test_juego_guerra.py
+++ TP1_juego_guerra/pruebas/test_juego_guerra.py
@@ -17,13 +17,19 @@ class TestMazo(unittest.TestCase):
 # Chequeo de repartir/poner arriba, sacar arriba/principio y poner abajo/final
     def test_poner_sacar_arriba(self):            #Poner arriba al momento de repartir las cartas, sacar arriba al momento de jugar el turno
         carta1=Carta('5','trebol')
+        carta2=Carta('3','corazones')
         self.mazo.poner_arriba(carta1)
-        self.assertIs(carta1, self.mazo.mazo.items[0])
-        carta2=self.mazo.sacar_arriba()
-        self.assertIs(carta1, carta2)
+        self.mazo.poner_arriba(carta2)
+        self.assertIs(carta2, self.mazo.mazo.items[0])
+        carta_control=self.mazo.sacar_arriba()
+        self.assertIs(carta2, carta_control)
 
-# def test_poner_abajo(self):                     #Poner abajo al momento de ganar el turno
-        
+    def test_poner_abajo(self):                     #Poner abajo al momento de ganar el turno
+        carta1=Carta('5','trebol')
+        carta2=Carta('3','corazones')
+        self.mazo.poner_abajo(carta1)
+        self.mazo.poner_abajo(carta2)
+        self.assertIs(carta2, self.mazo.mazo.items[-1])
         
         
 if __name__ == '__main__':
